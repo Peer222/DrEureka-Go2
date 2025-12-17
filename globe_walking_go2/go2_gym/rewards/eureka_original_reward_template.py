112efcf5544e894f0101fefffd6752a3c2af90c1
Peer222 <pebadu@gmail.com>
2025-12-17 12:11:20 +0100

    globe walking go2: changed reward template

--- globe_walking_go2/go2_gym/rewards/eureka_original_reward_template.py
+++ globe_walking_go2/go2_gym/rewards/eureka_original_reward_template.py
@@ -3,17 +3,15 @@ import numpy as np
 from globe_walking.go1_gym.utils.math_utils import quat_apply_yaw, wrap_to_pi, get_scale_shift
 from isaacgym.torch_utils import *
 
-class EurekaRewardStub():
+class EurekaReward():
     def __init__(self, env):
         self.env = env
 
     def load_env(self, env):
         self.env = env
+    
+# INSERT EUREKA REWARD HERE
 
     # Success criteria as episode length
     def compute_success(self):
         return torch.ones_like(self.env.base_pos[:, 2])
-
-class EurekaReward(EurekaRewardStub):
-
-# INSERT EUREKA REWARD HERE
